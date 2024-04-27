374af6c69560dbe3cc4a3b6bab7a09b9088d06f3
Ridwan4D <ridwanul161@gmail.com>
2024-04-27 16:08:10 +0600

    mongodb connected with environment variable

--- index.js
+++ index.js
@@ -32,12 +32,19 @@ async function run() {
         const userCollection = client.db("touristSpotDB").collection("user");
 
 
-        app.get("users",async(req,res)=>{
-            const id = req.params.id;
-            console.log(id);
-            const query = {_id: new ObjectId(id)}
-            // const result = await userCollection.findOne(query);
-        })
+
+
+
+
+
+        // user related apis
+        // app.get('/users/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log("please show the id: ", id);
+        //     const query = { _id: new ObjectId(id) }
+        //     const user = await userCollection.findOne(query);
+        //     res.send(user);
+        // })
 
         app.post("/users",async(req,res)=>{
             const user = req.body;
